3750feecd00db267c3f809b163909cc832a3ed78
Simon Taylor <Simonwtaylor93@gmail.com>
2021-10-10 12:27:59 +0100

    Cleaned up sidebar

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    apiURL: 'http://localhost:5001',
+  },
 }
